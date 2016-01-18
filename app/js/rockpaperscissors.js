c76b8c95f22eaf566396c4b3cfa3ef98e3e2e179
Sara S Horowitz <sarashorowitz@Sara-Lynchs-MacBook-Pro-2.local>
2016-01-18 16:14:56 -0500

    added switch for winner function

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -40,7 +40,44 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+    switch (playerMove) {
+        case "rock":
+        if (computerMove == "scissors") {
+            winner = "player";
+        } 
+        else if (computerMove == "paper") {
+            winner = "computer";
+        } 
+        else {
+            winner = "tie";
+        }
+        break;
+        case "paper":
+        if (computermove == "rock") {
+            winner = "player";
+        }
+        else if (computerMove == "scissors") {
+            winner = "computer";
+        }
+        else {
+            winner = "tie";
+        }
+        break;
+        case "scissors":
+        if (computerMove == "paper") {
+            winner = "player";
+        }
+        else if (computerMove == "rock") {
+            winner = "computer";
+        }
+        else {
+            winner = "tie";
+        }
+        break;
+        default:
+        winner = "tie";
+    }
+    
     return winner;
 }
 
