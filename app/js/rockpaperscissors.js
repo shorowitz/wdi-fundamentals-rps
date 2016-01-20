6f49af92aa1c0a5538d8366e80f7067420a59118
Sara S Horowitz <sarashorowitz@Sara-Lynchs-MacBook-Pro-2.local>
2016-01-19 20:12:49 -0500

    playToFive executing infinite loop - stuck

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -40,7 +40,7 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    if ((playerMove === "rock")&&(computerMove === "scissors") {
+    if ((playerMove === "rock")&&(computerMove === "scissors")) {
         winner= "player";
         console.log("Player Wins");
     }
@@ -76,7 +76,26 @@ function playToFive() {
     var playerWins = 0;
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
+var playerMove = "";
+var computerMove = "";
+var roundWinner = "";
+
+while ((playerWins < 5)&&(computerWins < 5)) {
+        playerMove = getPlayerMove(getInput());
+        computerMove= getComputerMove(randomPlay());
+        console.log("Player chose " + playerMove + " while Computer chose " + computerMove);
+        
+        roundWinner = getWinner(playerMove, computerMove);
+        console.log("The score is currently " + playerWins + " to " + computerWins);
+
+        if (playerMove === roundWinner) {
+            playerWins += 1;
+        }
+        else if (computerMove === roundWinner) {
+            computerWins += 1;
+        }
+
+    }
     return [playerWins, computerWins];
 }
 
